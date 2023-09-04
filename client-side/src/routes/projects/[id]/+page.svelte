357114bd49282916528fb56db270cc23ee548714
08Miguel24 <MichaelAveuc571@gmail.com>
2023-09-04 13:51:36 +0800

    updated to svelte kit for routing

--- client-side/src/routes/projects/[id]/+page.svelte
+++ client-side/src/routes/projects/[id]/+page.svelte
@@ -1,6 +1,15 @@
 <script>
     import { page } from "$app/stores";
+
+    // $page params contains a dicitonary of all the parameters in our
+    // url e.g. a url of projects/[id] or a url of projects/[id]/[desc]
+    // or even a url of projects/[id]/[image].png will all return a dictionary
+    // that has keys set as the name of the parameter and the value set as the
+    // value we passed that aligns to this parameter so if projects/1
+    // (% indicating a whitespace) then $page.params will be {id: 1}
+    const {id: project_id} = $page.params;
+
 </script>
 <h1>
-    Project {page.id}
+    Project {project_id}
 </h1>
