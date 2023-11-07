33eaef5a2ecd8acfed1ce40d294b920d77454267
08Miguel24 <MichaelAveuc571@gmail.com>
2023-11-07 21:51:17 +0800

    update

--- client-side/src/app/layout.js
+++ client-side/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar brand="LMC Engineering Front"/>
+        <Navbar brand="LMC-EF"/>
         <main className="main-content">
           {children}
         </main>
--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-// import { useState } from "react";
+"use client";
+
+import { useState } from "react";
 
 export default function Navbar(props){
-    // let [isOpened, setIsOpened] = useState(false);
+    let [isOpened, setIsOpened] = useState(false);
 
     return (
-        <header className="navbar-container">
+        <header className={isOpened === true ? "navbar-container opened" : "navbar-container"}>
             <nav className="navbar">
                 <div className="nav-brand-container">
                     <a className="navbar-brand" href="/">{props['brand']}</a>
                     
-                    <div className="button-container">
+                    <div className={isOpened === true ? "button-container opened" : "button-container"} onClick={() => {
+                        setIsOpened((state) => !state);
+                    }}>
                         <span className="bar"></span>
                     </div>
                 </div>
@@ -17,7 +21,8 @@ export default function Navbar(props){
                 <div className="nav-menu-container">
                     <div className="nav-menu">
                         <a className="nav-item about-section" aria-current="page">about</a>
-                        <a className="nav-item work-group-section">work</a>
+                        <a className="nav-item projects-section" aria-current="page">projects</a>
+                        <a className="nav-item services-section">services</a>
                         <a className="nav-item contact-section">contact</a>
                     </div>
                 </div>
