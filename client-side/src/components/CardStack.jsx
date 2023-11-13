f741883d6bc232e8de19c2b930d299cee76f0f25
08Miguel24 <MichaelAveuc571@gmail.com>
2023-11-13 15:07:33 +0800

    reengineered stack animation

--- client-side/src/components/CardStack.jsx
+++ client-side/src/components/CardStack.jsx
@@ -1,27 +1,48 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+
+
+
 export default function CardStack(){
-    const cards = Array.from({length: 3}, (_, i) => i + 1).map((value, index) => {
-      return (
-        <div className="card" key={index}>
-          <div className="card-headers">
-            <h2>{value}</h2>
-          </div>
-          <div className="card-content">
-            <ul>
-                <li>test</li>
-                <li>test</li>
-                <li>test</li>
-            </ul>
-          </div>
-          <img className="card-background" src={`/compressed/portrait-stocks (${value}).jpg`} alt={`stock-image ${value}`}/>
-        </div>
-      );
-    });
-  
+  let [i, setI] = useState(0);
+  let active = 3;
+
+  const card_arr = Array.from({length: 3}, (_, i) => i + 1).map((value, index) => {
     return (
-      <div className="cards" data-active="3" data-current="3">
-        {cards}
+      <div className="card" key={index}>
+        <div className="card-headers">
+          <h2>{value}</h2>
+        </div>
+        <div className="card-content">
+          <ul>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+          </ul>
+        </div>
+        <img className="card-background" src={`/compressed/portrait-stocks (${value}).jpg`} alt={`stock-image ${value}`}/>
       </div>
     );
+  });
+
+  const cards = useRef();
+
+  useEffect(() => {
+    setInterval(() => {
+      setI((state) => state + 1);
+      active = i % 3 + 1;
+      cards.current.removeAttribute('data-current');
+      
+      setTimeout(() => {
+        cards.current.setAttribute('data-current', active);
+      }, 1000);
+    }, 3000)
+  }, []);
+
+  return (
+    <div className="cards" data-active={active} data-current={active} ref={cards}>
+      {card_arr}
+    </div>
+  );
 }
