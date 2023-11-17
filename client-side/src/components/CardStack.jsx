ac7f2286c06a9bf1107e97ee54a89bf3b3dbdfe4
08Miguel24 <MichaelAveuc571@gmail.com>
2023-11-17 20:55:01 +0800

    update

--- client-side/src/components/CardStack.jsx
+++ client-side/src/components/CardStack.jsx
@@ -11,26 +11,26 @@ export default class CardStack extends Component{
  }
 
  componentDidMount(){
-  // const cards = document.querySelector('.cards');
+  const cards = document.querySelector('.cards');
 
-  // if(cards !== null){
-  //   let i = 0;
-  //   this.interval = setInterval(() => {
-  //     this.activeIndex['current'] = (i++ % 3) + 1;
+  if(cards !== null){
+    let i = 0;
+    this.interval = setInterval(() => {
+      this.activeIndex['current'] = (i++ % 6) + 1;
   
-  //     cards.setAttribute('data-active', this.activeIndex['current']);
-  //     cards.removeAttribute('data-current');
+      cards.setAttribute('data-active', this.activeIndex['current']);
+      cards.removeAttribute('data-current');
   
-  //     setTimeout(() => {
-  //       cards.setAttribute('data-current', this.activeIndex['current']);
-  //     }, 1000);
-  //   }, 5000);
-  // }
+      setTimeout(() => {
+        cards.setAttribute('data-current', this.activeIndex['current']);
+      }, 1000);
+    }, 5000);
+  }
  }
 
  render(){
 
-  const card_arr = Array.from({length: 3}, (_, i) => i + 1).map((value, index) => {
+  const card_arr = Array.from({length: 6}, (_, i) => i + 1).map((value, index) => {
     return (
       <div className="card" key={index}>
         <div className="card-headers">
