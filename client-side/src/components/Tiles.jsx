21ffe04f86349545d8b4fe699d54b0e16b1b5f21
08Miguel24 <MichaelAveuc571@gmail.com>
2023-11-29 20:01:40 +0800

    update

--- client-side/src/components/Tiles.jsx
+++ client-side/src/components/Tiles.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Script from "next/script";
+import '../assets/stylesheets/Fancy-Responsive-jQuery-Diamond-Layout-Plugin-diamonds-js/src/diamonds.css';
+
 export default function Tiles(){
     const tile_names = ['Mechanical', 'Civil', 'Electrical', 'Structural'];
 
